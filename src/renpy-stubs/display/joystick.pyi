7a2b7ec378e6912c9323efd019ae966c65f225dc
Arsym <123092893+arsym-dev@users.noreply.github.com>
2025-12-17 16:43:06 -0500

    renpy.display.joystick

--- src/renpy-stubs/display/joystick.pyi
+++ src/renpy-stubs/display/joystick.pyi
@@ -1,12 +1,13 @@
 import renpy
 from _typeshed import Incomplete as Incomplete
 from renpy.display.layout import Null as Null
+from renpy.pygame.joystick import Joystick as Joystick
 
 enabled: bool
 
 class JoyBehavior(renpy.display.layout.Null): ...
 
-joysticks: Incomplete
+joysticks: dict[int, Joystick]
 
-def count(): ...
-def get(n): ...
+def count() -> int: ...
+def get(n: int) -> Joystick | None: ...
