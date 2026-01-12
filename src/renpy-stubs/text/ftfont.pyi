767ff2243fe549193f09a6648d04990e1c1b561f
Arsym <123092893+arsym-dev@users.noreply.github.com>
2026-01-12 02:56:37 -0500

    fonts

--- src/renpy-stubs/text/ftfont.pyi
+++ src/renpy-stubs/text/ftfont.pyi
@@ -1,6 +1,7 @@
 from _frozen_importlib import BuiltinImporter as BuiltinImporter
-from _typeshed import Incomplete
-from types import IO
+from typing import IO
+from renpy.text.textsupport import Glyph
+from renpy.pygame.surface import Surface
 
 class FTFace:
     def __init__(self, f: IO, index: int, fn: str) -> None: ...
@@ -17,21 +18,21 @@ class FTFont:
         vertical: bool,
         hinting: str | None,
     ) -> None: ...
-    def bounds(self, glyphs: Incomplete, bounds: Incomplete) -> Incomplete: ...
+    def bounds(self, glyphs: list[Glyph], bounds: tuple[int, int, int, int]) -> tuple[int, int, int, int]: ...
     def draw(
         self,
-        pysurf: Incomplete,
+        pysurf: Surface,
         xo: float,
         yo: int,
-        color: Incomplete,
+        color: tuple[int, int, int, int],
         glyphs: list,
         underline: int,
         strikethrough: bool,
-        black_color: Incomplete,
-    ) -> Incomplete: ...
-    def glyphs(self, s: str, level: int) -> Incomplete: ...
+        black_color: tuple[int, int, int, int] | None,
+    ) -> None: ...
+    def glyphs(self, s: str, level: int) -> list[Glyph]: ...
 
 class FreetypeError(Exception):
-    def __init__(self, code: Incomplete) -> None: ...
+    def __init__(self, code: int) -> None: ...
 
 def init() -> None: ...
--- src/renpy-stubs/text/hbfont.pyi
+++ src/renpy-stubs/text/hbfont.pyi
@@ -1,18 +1,20 @@
 from _frozen_importlib import BuiltinImporter as BuiltinImporter
-from _typeshed import Incomplete
-from typing import IO
+from typing import IO, Literal
+from renpy.pygame.surface import Surface
+from renpy.text.textsupport import Glyph
+
+type FeatureType = tuple[str, int]
 
 class Axis:
-    def __init__(self, index: Incomplete, minimum: Incomplete, default: Incomplete, maximum: Incomplete) -> None: ...
+    def __init__(self, index: int, minimum: float, default: float, maximum: float) -> None: ...
 
 class Features:
-    def __init__(self, features: Incomplete) -> None: ...
-    def __reduce__(self) -> str | tuple[Any, ...]: ...
+    def __init__(self, features: list[FeatureType]) -> None: ...
     @staticmethod
-    def get(features: Incomplete) -> None: ...
+    def get(features: list[FeatureType]) -> Features: ...
 
 class FreetypeError(Exception):
-    def __init__(self, code: Incomplete) -> None: ...
+    def __init__(self, code: int) -> None: ...
 
 class HBFace:
     def __init__(self, f: IO, index: int, fn: str) -> None: ...
@@ -28,23 +30,23 @@ class HBFont:
         antialias: bool,
         vertical: bool,
         hinting: str | None,
-        instance: Incomplete,
-        axis: Incomplete,
-        features: Incomplete,
+        instance: Literal["bold", "bold italic", "regular", "italic"] | None,
+        axis: dict[str, Axis] | None,
+        features: list[FeatureType] | None,
     ) -> None: ...
-    def bounds(self, glyphs: Incomplete, bounds: Incomplete) -> Incomplete: ...
+    def bounds(self, glyphs: list[Glyph], bounds: tuple[int, int, int, int]) -> tuple[int, int, int, int]: ...
     def draw(
         self,
-        pysurf: Incomplete,
+        pysurf: Surface,
         xo: float,
         yo: int,
-        color: Incomplete,
-        glyphs: list,
+        color: tuple[int, int, int, int],
+        glyphs: list[Glyph],
         underline: int,
         strikethrough: bool,
-        black_color: Incomplete,
-    ) -> Incomplete: ...
-    def glyphs(self, s: str, level: int) -> Incomplete: ...
+        black_color: tuple[int, int, int, int] | None,
+    ) -> None: ...
+    def glyphs(self, s: str, level: int) -> list[Glyph]: ...
 
 class Variations:
     def __init__(self) -> None: ...
